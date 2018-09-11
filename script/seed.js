c777808175587ec8a768d148c443f86210a826ea
Megha Patel <meghs2512@gmail.com>
2018-09-11 15:26:49 -0400

    add seed file

--- script/seed.js
+++ script/seed.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const db = require('../server/db');
-const { User } = require('../server/db/models');
+const { User, Address, Category, Order, OrderProduct, Review, Role, Type } = require('../server/db/models');
 
 async function seed() {
 	await db.sync({ force: true });
@@ -48,17 +48,183 @@ async function seed() {
 			password: '109'
 		}
 	];
-	const users = await Users.map((user) => {
-		User.create({
-			firstName: user.firstName,
-			lastName: user.lastName,
-			phoneNumber: user.phoneNumber,
-			username: user.username,
-			email: user.email,
-			password: user.password
-		});
-	});
+	const Addresses = [
+		{
+			address: '1809 Plaza Dr',
+			city: 'Woodbridge',
+			state: 'NJ',
+			country: 'USA',
+			zipcode: '07008',
+			isPrimary: true
+		},
+		{
+			address: '41 Par Harbor Way',
+			city: 'Salem',
+			state: 'SC',
+			country: 'USA',
+			zipcode: '29676',
+			isPrimary: false
+		},
+		{
+			address: '2339 Macungie Dr',
+			city: 'Macungie',
+			state: 'PA',
+			country: 'USA',
+			zipcode: '18062',
+			isPrimary: true
+		},
+		{
+			address: '18 Canal View Dr',
+			city: 'WaynesVille',
+			state: 'NC',
+			country: 'USA',
+			zipcode: '28738',
+			isPrimary: true
+		},
+		{
+			address: ' 45 Main St',
+			city: 'Woodbridge',
+			state: 'NJ',
+			country: 'USA',
+			zipcode: '07008',
+			isPrimary: false
+		}
+	];
+	const Categories = [
+		{
+			name: 'Tea Bags',
+			description:
+				'A tea bag is a small, porous, sealed bag or packet containing dried plant material, which is immersed in boiling water to make a tea or an infusion.'
+		},
+		{
+			name: 'Loose Leaf Tea',
+			description:
+				'Tea is an aromatic beverage commonly prepared by pouring hot or boiling water over cured leaves of the Camellia sinensis, an evergreen shrub(bush) native to Asia.'
+		},
+		{
+			name: 'Iced Tea',
+			description:
+				'Iced tea (or ice tea) is a form of cold tea. Though usually served in a glass with ice, it can refer to any tea that has been chilled or cooled. It may be sweetened with sugar, syrup and/or apple slices.'
+		}
+	];
+	const Orders = [
+		{
+			status: ' ordered',
+			tracking: 'GS4536',
+			stripeToken: 'tok_1AH2kcF89KLdg'
+		},
+		{
+			status: 'shipped',
+			tracking: 'GS3474',
+			stripeToken: 'tok_Efu42LDeF89KL'
+		},
+		{
+			status: 'delivered',
+			tracking: 'GS1000',
+			stripeToken: 'tok_i56WoP90BgDt4'
+		},
+		{
+			status: 'delivered',
+			tracking: 'GS8965',
+			stripeToken: 'tok_i45GJk68NOe90'
+		},
+		{
+			status: 'ordered',
+			tracking: 'GS1078',
+			stripeToken: 'tok_yO98JgHTR10dJ'
+		}
+	];
+	const Reviews = [
+		{
+			rating: 4,
+			content: 'The most delicious things to touch my tongue.'
+		},
+		{
+			rating: 2,
+			content: 'Bitter and too spicy!!!!'
+		},
+		{
+			rating: 5,
+			content: 'Hit the spot'
+		},
+		{
+			rating: 3,
+			content: 'I feel cleansed and pure but wish there was a stronger flavor'
+		},
+		{
+			rating: 4,
+			content:
+				'Brewed some of this and squeezed it across the terrace to combat the ants, worked as planned though some stronger ones remained alive.'
+		}
+	];
+	const Roles = [
+		{
+			role: 'admin'
+		},
+		{
+			role: 'customer'
+		},
+		{
+			role: 'admin'
+		},
+		{
+			role: 'customer'
+		}
+	];
+	const Types = [
+		{
+			name: 'black tea',
+			caffeineStrength: 'strong'
+		},
+		{
+			name: 'green tea',
+			caffeineStrength: 'weak'
+		},
+		{
+			name: 'white tea',
+			caffeineStrength: 'mild'
+		},
+		{
+			name: 'herbal tea',
+			caffeineStrength: 'none'
+		}
+	];
 
+	const users = await Promise.all(
+		Users.map((user) => {
+			User.create(user);
+		})
+	);
+	const addresses = await Promise.all(
+		Addresses.map((address) => {
+			Address.create(address);
+		})
+	);
+	const categories = await Promise.all(
+		Categories.map((category) => {
+			Category.create(category);
+		})
+	);
+	const orders = await Promise.all(
+		Orders.map((order) => {
+			Order.create(order);
+		})
+	);
+	const reviews = await Promise.all(
+		Reviews.map((review) => {
+			Review.create(review);
+		})
+	);
+	const roles = await Promise.all(
+		Roles.map((role) => {
+			Role.create(role);
+		})
+	);
+	const types = await Promise.all(
+		Types.map((type) => {
+			Type.create(type);
+		})
+	);
 	console.log(`seeded ${users.length} users`);
 	console.log(`seeded successfully`);
 }
