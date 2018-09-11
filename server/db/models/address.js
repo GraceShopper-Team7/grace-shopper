8166f4a230d572ceca87e74212ad1cf75676734f
Siri McClean <nmcclean@wellesley.edu>
2018-09-11 13:06:44 -0400

    adds the following sequelize models: address, category, order, orderProduct, review, role, type. also, makes changes to the previously created user sequelize model.

---
+++ server/db/models/address.js
@@ -0,0 +1,45 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Address = db.define('address', {
+  address: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isAlphanumeric: true,
+    },
+  },
+  city: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isAlpha: true,
+    },
+  },
+  state: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isAlpha: true,
+    },
+  },
+  country: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isAlpha: true,
+    },
+  },
+  zipcode: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isNumeric: true,
+    },
+  },
+  isPrimary: {
+    type: Sequelize.BOOLEAN,
+  },
+});
+
+module.exports = Address;
---
+++ server/db/models/orderProduct.js
@@ -0,0 +1,22 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const OrderProduct = db.define('orderProduct', {
+  quantity: {
+    type: Sequelize.INTEGER,
+    defaultValue: 1,
+    validate: {
+      min: 0,
+      max: 1000,
+    },
+  },
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+});
+
+module.exports = OrderProduct;
