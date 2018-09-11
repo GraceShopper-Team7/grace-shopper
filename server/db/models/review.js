f5c4ff849dabe8c19aa919e4b22cfa3ef9326d12
Siri McClean <nmcclean@wellesley.edu>
2018-09-11 15:44:19 -0400

    prettier commit

--- server/db/models/review.js
+++ server/db/models/review.js
@@ -7,16 +7,17 @@ const Review = db.define('review', {
     allowNull: false,
     validate: {
       min: 0,
-      max: 5,
-    },
+      max: 5
+    }
   },
   content: {
     type: Sequelize.TEXT(medium),
+
     allowNull: false,
     validate: {
-      notEmpty: true,
-    },
-  },
-});
+      notEmpty: true
+    }
+  }
+})
 
-module.exports = Review;
+module.exports = Review
