9241f49b6f180cfba08b3b970931e781c5701aa1
Siri McClean <nmcclean@wellesley.edu>
2018-09-13 11:08:05 -0400

    working on cart

--- client/store/cart.js
+++ client/store/cart.js
@@ -3,11 +3,20 @@ import axios from 'axios'
 //ACTIONS
 const GET_ORDERPRODUCTS_FROM_SERVER = 'GET_ORDERPRODUCTS_FROM_SERVER'
 
+const ADD_ORDERPRODUCT_TO_SERVER = 'ADD_ORDERPRODUCT_TO_SERVER'
+
 //ACTION CREATORS
 export const setOrderProductsInStore = function(products) {
   return {
     type: GET_ORDERPRODUCTS_FROM_SERVER,
     products
+  }
+}
+
+export const addOrderProductsInStore = function(product) {
+  return {
+    type: ADD_ORDERPRODUCT_TO_SERVER,
+    product
   }
 }
 
@@ -19,12 +28,19 @@ export const fetchOrderProducts = () => {
     let products = res.data
     const action = setOrderProductsInStore(products)
     dispatch(action)
+  }
+}
 
-    // let res = await axios.get('/api/products')
-    // let products = res.data
-    // const action = setProductsInStore(products)
-    // console.log("I'm done fetching..., let's dispatch to the store")
-    // dispatch(action)
+export const addOrderProducts = () => {
+  return async dispatch => {
+    //dispatch(setIsLoading())
+    //should this be post?!?! maybe not!!!
+    //need to remove item from store and
+    //take that item and create a new orderProduct for it
+    let res = await axios.post('/api/orderProducts')
+    let product = res.data
+    const action = addOrderProductsInStore(product)
+    dispatch(action)
   }
 }
 
@@ -43,6 +59,11 @@ const cartReducer = (
       return {
         ...state,
         all: action.products
+      }
+    case ADD_ORDERPRODUCT_TO_SERVER:
+      return {
+        ...state,
+        all: [...state.all, action.product]
       }
     default:
       return state
